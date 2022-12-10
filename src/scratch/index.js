66cd6b5b6773033eecfa9368e401dc25467e1ce8
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2022-12-10 00:54:09 -0800

    intro to state machine

--- src/scratch/index.js
+++ src/scratch/index.js
@@ -2,7 +2,57 @@ import React from 'react';
 import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
+
+const initialState = 'inactive'
+const alarmMachine = {
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        TOGGLE: 'pending',
+      }
+    },
+    pending: {
+      on: {
+        SUCCESS: 'active',
+        TOGGLE: 'inactive'
+      }
+    },
+    active: {
+      on: {
+        TOGGLE: 'inactive'
+      }
+    },
+  }
+}
+const timeReducer = (state, event) => {
+  const nextState = alarmMachine.states[state].on[event.type] || state
+  return nextState
+  // switch (state) {
+  //   case 'inactive': {
+  //     if(event.type==='TOGGLE') return 'pending'
+  //     return state
+  //   }
+  //   case 'pending': {
+  //     if (event.type === 'SUCCESS') return 'active'
+  //     if(event.type ==='TOGGLE') return 'inactive'
+  //     return state
+  //   }
+  //   case 'active': {
+  //     if(event.type === 'TOGGLE') return 'inactive'
+  //     return state;
+  //   }
+  //   default: return state;
+  // }
+}
 export const ScratchApp = () => {
+  const [state,dispatch] = React.useReducer(timeReducer,initialState)
+  console.log({state})
+
+  React.useEffect(() => { 
+    const timer = setTimeout(() => dispatch({ type: 'SUCCESS' }), 3000)
+    return ()=> clearTimeout(timer)
+  },[state])
   return (
     <div className="scratch">
       <div className="alarm">
@@ -12,7 +62,7 @@ export const ScratchApp = () => {
             minute: '2-digit',
           })}
         </div>
-        <div className="alarmToggle" data-active></div>
+        <div className="alarmToggle" data-active={state==='active' || undefined} onClick={()=>dispatch({type:'TOGGLE'})}></div>
       </div>
     </div>
   );
